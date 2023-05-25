f488126bea27b8dbe30449598146053f10044663
Birko Bergt <Birko.Bergt@gmail.com>
2023-05-25 16:00:34 +0200

    Add Neff and R101 values

--- dwd-opendata-kml.py
+++ dwd-opendata-kml.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # parse dwd opendata weather forecast
 #
 # Dirk Clemens, git@adcore.de 
 #
-# https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/10400/kml/MOSMIX_L_LATEST_10400.kmz
+# https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/E970/kml/MOSMIX_L_LATEST_E970.kmz
 #
 ############################################################
 # DWD Wetter
@@ -31,6 +32,95 @@ import re
 import dateutil.parser
 from io import BytesIO
 from urllib.request import urlopen
+
+import json
+import time
+import datetime
+import calendar
+from os.path import expanduser
+from influxdb import InfluxDBClient
+
+region        = 'E970'     # Dransfeld-Ossenfeld
+
+dbHost        = '192.168.188.47'
+dbPort        = 8086
+dbUser        = 'admin'
+dbPasswd      = ''
+dbName        = 'enverbridge'
+dbMeasurement = 'dwdweather'
+dbToken       = ''
+influxTag     = 'mosmix'
+influxTopic   = 'weather/dwd/mosmix'
+
+#      Länge * Breite * Anzahl
+area = 1.675 * 0.992 * 4.0
+# Wirkungsgrad: Modul  * Wechselrichter * senkrechte Aufstellung
+efficiency = 0.1926 * 0.956 * 0.7
+
+#debug = True
+debug = False
+
+# Globalstrahlung * 0,278 (Umrechnung in Watt/m2) * Modulfläche * Wirkungsgrad
+def rad1hToWatt(rad1h):
+    return float(rad1h * 0.2777777777777778 * area * efficiency)
+
+datePattern = "%Y-%m-%dT%H:%M:%S.%f"
+
+def readPasswdFromFile(passwdFileName):
+    if passwdFileName.startswith('~'):
+        passwdFileName = expanduser(passwdFileName)
+    pfile = open(passwdFileName, 'r')
+    pw = pfile.readline()
+    pfile.close()
+
+    return str(pw.rstrip("\n"))
+
+def toJson(dateStr, timeStamp, rad1h, ff, neff):
+    if (debug): print("toJson(%s, %s, %s, %s)" % (dateStr, timeStamp, rad1h, ff))
+    result = {
+        "Date":                dateStr,
+        "Timestamp":           timeStamp,
+        "Rad1h":               float(rad1h),
+        "ExpectedPower":       round(rad1hToWatt(float(rad1h)), 3),
+        "Area":                round(area, 3),
+        "Efficiency":          round(efficiency, 3),
+        "Windspeed":           round(ff, 3),
+        "EffectiveCloudCover": round(neff, 3),
+    }
+
+    return result
+
+def updateInflux(region, timeStamp, data):
+    if (debug): print("updateInflux(%s %s %s)" % (region, timeStamp, json.dumps(data)))
+
+    global dbPasswd
+    dbPasswd = readPasswdFromFile("~/.db_passwd")
+
+    client = InfluxDBClient(dbHost, dbPort, dbUser, dbPasswd, dbName)
+
+    points = [
+        {
+            "time": timeStamp,
+            "measurement": dbMeasurement,
+            "tags": {
+                "host":   "tuya",
+                "topic":  influxTopic,
+                "region": region
+            },
+            "fields": data
+        }
+    ]
+
+#    print("updateInflux: ", json.dumps(points))
+
+    # precision is in seconds
+    time_precision = 's'
+
+    # write the datapoint
+    client.write_points(
+        points = points,
+        time_precision = time_precision
+    )
 
 def numeric(s):
     try:
@@ -66,61 +156,81 @@ def analyse(tree):
 
     #<dwd:IssueTime>2018-12-18T15:00:00.000Z</dwd:IssueTime>
     for df in tree.xpath('////*[name()="dwd:IssueTime"]'):
-        print (dateutil.parser.parse(df.text).__format__("%d.%m.%Y %H:%M"))
+        print (dateutil.parser.parse(df.text).__format__(datePattern))
+        # print (dateutil.parser.parse(df.text).__format__("%d.%m.%YT%H:%M:%S.%fZ"))
 
-    print ('\n')
+    #print ('\n')
 
     ele_TimeStamp = []
     for df in tree.xpath('//*[name()="dwd:ForecastTimeSteps"]'):
         timeslots = df.getchildren()
         for timeslot in timeslots:
             # print ('timeslot=' + timeslot.text)
-            tm = dateutil.parser.parse(timeslot.text).__format__("%d.%m.%Y %H:%M")
+            # tm = dateutil.parser.parse(timeslot.text).__format__("%d.%m.%YT%H:%M:%S.%fZ")
+            tm = dateutil.parser.parse(timeslot.text).__format__(datePattern)
             #tm = timeslot.text
             ele_TimeStamp.append(tm)
-    print("Time", ele_TimeStamp)
+#    print("Time (", len(ele_TimeStamp), ") ", ele_TimeStamp)
 
-    ele_PPPP = getElementValueAsList(tree, 'PPPP')  # =x/100
-    for index, item in enumerate(ele_PPPP):
-        ele_PPPP[index] = float(ele_PPPP[index]) / 100.0
-    print("PPPP", ele_PPPP)
+    ele_Rad1h = getElementValueAsList(tree, 'Rad1h')
+    if (debug): print("Rad1h (", len(ele_Rad1h), ") ", ele_Rad1h)
 
-    ele_FX1 = getElementValueAsList(tree, 'FX1')
-    print("FX1 ", ele_FX1)
+    ele_FF = getElementValueAsList(tree, 'FF')
+    if (debug): print("FF (", len(ele_FF), ") ", ele_FF)
 
-    ele_ww = getElementValueAsList(tree, 'ww')
-    print("ww  ", ele_ww)
-
-    ele_SunD = getElementValueAsList(tree, 'SunD')  # =round(x)
-    for index, item in enumerate(ele_SunD):
-        ele_SunD[index] = round(float(ele_SunD[index]), 2)
-    print("SunD", ele_SunD)
-
-    ele_TX = getElementValueAsList(tree, 'TX')  # =x-273.15
-    for index, item in enumerate(ele_TX):
-        if (int(ele_TX[index]) > 99):
-            ele_TX[index] = round(float(ele_TX[index]) - 273.15, 2)
-    print("TX  ", ele_TX)
-
-    ele_Tn = getElementValueAsList(tree, 'TN')  # =x-273.15
-    for index, item in enumerate(ele_Tn):
-        if (int(ele_Tn[index]) > 99):
-            ele_Tn[index] = round(float(ele_Tn[index]) - 273.15, 2)
-    print("Tn  ", ele_Tn)
-
+#    ele_PPPP = getElementValueAsList(tree, 'PPPP')  # =x/100
+#    for index, item in enumerate(ele_PPPP):
+#        ele_PPPP[index] = float(ele_PPPP[index]) / 100.0
+#    print("PPPP (", len(ele_PPPP), ") ", ele_PPPP)
+#
+#    ele_FX1 = getElementValueAsList(tree, 'FX1')
+#    print("FX1 (", len(ele_FX1), ") ", ele_FX1)
+#
+#    ele_ww = getElementValueAsList(tree, 'ww')
+#    print("ww  (", len(ele_ww), ") ", ele_ww)
+#
+#    ele_SunD = getElementValueAsList(tree, 'SunD')  # =round(x)
+#    for index, item in enumerate(ele_SunD):
+#        ele_SunD[index] = round(float(ele_SunD[index]), 2)
+#    print("SunD (", len(ele_SunD), ") ", ele_SunD)
+#
+#    ele_TX = getElementValueAsList(tree, 'TX')  # =x-273.15
+#    for index, item in enumerate(ele_TX):
+#        if (int(ele_TX[index]) > 99):
+#            ele_TX[index] = round(float(ele_TX[index]) - 273.15, 2)
+#    print("TX  (", len(ele_TX), ") ", ele_TX)
+#
+#    ele_Tn = getElementValueAsList(tree, 'TN')  # =x-273.15
+#    for index, item in enumerate(ele_Tn):
+#        if (int(ele_Tn[index]) > 99):
+#            ele_Tn[index] = round(float(ele_Tn[index]) - 273.15, 2)
+#    print("Tn  (", len(ele_Tn), ") ", ele_Tn)
+#
     ele_Neff = getElementValueAsList(tree, 'Neff')  # =x*8/100
     for index, item in enumerate(ele_Neff):
         ele_Neff[index] = float(ele_Neff[index]) * 8 / 100
-    print("Neff", ele_Neff)
+    if (debug): print("Neff (", len(ele_Neff), ") ", ele_Neff)
+#
+#    ele_R101 = getElementValueAsList(tree, 'R101')
+#    print("R101 (", len(ele_R101), ") ", ele_R101)
 
-    print("R101", getElementValueAsList(tree, 'R101'))
-
+    for index, item in enumerate(ele_TimeStamp):
+        dt = datetime.datetime.strptime(item, datePattern).timetuple()
+        #dtLocal = calendar.timegm(dt)
+        dtLocal = int(time.mktime(dt))
+        rad1h = ele_Rad1h[index]
+        ff = ele_FF[index]
+        neff = ele_Neff[index]
+        if (debug): print("index: ", index, ", dateStr: ", item, ", timestamp: ", dtLocal, ", Rad1h: ", rad1h, " FF: ", ff, " Neff: ", neff)
+        updateInflux(region, dtLocal, toJson(item, dtLocal, rad1h, ff, neff))
 
 def go():
-    url = 'https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/10400/kml/MOSMIX_L_LATEST_10400.kmz'
+    url = 'https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/' + region + '/kml/MOSMIX_L_LATEST_' + region + '.kmz'
+    #url = 'https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/E970/kml/MOSMIX_L_2021110821_E970.kmz'
+    #url = 'http://192.168.1.134/MOSMIX/MOSMIX_L_2021110821_E970.kmz'
 
     kmz = zipfile.ZipFile(BytesIO(urlopen(url).read()), 'r')
-    #kmz = zipfile.ZipFile('MOSMIX_L_LATEST_10400.kmz', 'r')
+    #kmz = zipfile.ZipFile('MOSMIX_L_LATEST_E970.kmz', 'r')
 
     kml_filename = kmz.namelist()[0]
     #print (kml_filename)
@@ -134,5 +244,5 @@ def go():
 
 
 if __name__ == '__main__':
-    print ('\n')
+    #print ('\n')
     go()
